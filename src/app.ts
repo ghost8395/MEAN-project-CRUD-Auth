3ba822820fc4c2c0bf554e7b7d1bfe18101d7e68
ghost8395 <manikmudholkar831995@gmail.com>
2024-10-24 12:23:32 +0530

    Middle ware Packages

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,16 @@ import helmet from "helmet";
 import cors from "cors";
 import { notFound, errorHandler } from "./middleware/index.middleware";
 import api from "./api/index.api";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 config();
 
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
