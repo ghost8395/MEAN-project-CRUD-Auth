81dc7be6f4bbed5a81412ff3159947e57119a847
ghost8395 <manikmudholkar831995@gmail.com>
2024-10-24 11:39:10 +0530

    first route added

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
-
 import { notFound, errorHandler } from "./middleware/index.middleware";
+import api from "./api/index.api";
 
 config();
+
 
 const app = express();
 
@@ -15,5 +16,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/", api);
+
 app.use(notFound);
 app.use(errorHandler);
+
+export default app;
